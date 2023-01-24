8a64149c4d9ad2274439aca2f5e18c517f5baeeb
iain macdonald <loki@mail.com>
2023-01-24 16:52:14 +0000

    added for loop, saving and local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,37 @@
+//structure
 const container = document.querySelector(".container");
-const timeBlock = document.createElement('div')
-const div = document.createElement("div");
 
-const textarea = document.createElement("textarea");
+for (i = 9; i < 18; i++) {
+    const timeBlock = document.createElement('div')
+    const div = document.createElement("div");
 
-const button = document.createElement("button");
+    const textarea = document.createElement("textarea");
 
-timeBlock.setAttribute('class', 'time-block row')
+    const button = document.createElement("button");
+
+    timeBlock.setAttribute('class', 'time-block row')
 
 
-div.innerHTML = "9:00";
-div.setAttribute("class", "hour col-2")
-timeBlock.appendChild(div);
+    div.innerHTML = +i + ":00";
+    div.setAttribute("class", "hour col-2")
+    timeBlock.appendChild(div);
 
-textarea.innerHTML = "";
-textarea.setAttribute('class', 'col-8')
-timeBlock.appendChild(textarea);
+    textarea.innerHTML = "";
+    textarea.setAttribute('class', 'col-8')
+    textarea.value = localStorage.getItem(+i + ":00");
+    timeBlock.appendChild(textarea);
 
-button.innerHTML = "Save";
-button.setAttribute("class", "saveBtn col-2")
-timeBlock.appendChild(button);
+    button.innerHTML = "Save";
+    button.setAttribute("class", "saveBtn col-2")
+    timeBlock.appendChild(button);
 
-container.appendChild(timeBlock)
+    container.appendChild(timeBlock)
+
+    // save
+    button.addEventListener("click", function(event) {
+        const userInput = event.target.previousElementSibling.value;
+        const hour = div.textContent;
+        localStorage.setItem(hour, userInput);
+    })
+    ;
+}
